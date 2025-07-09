3b46c8cf07a34ef16d7b982431c9716b81dac4e7
icssoa <615206459@qq.com>
2025-07-09 20:57:40 +0800

    1

--- packages/vite-plugin/dist/uniapp-x/config.d.ts
+++ packages/vite-plugin/dist/uniapp-x/config.d.ts
@@ -2,3 +2,7 @@
  * 特殊字符映射表
  */
 export declare const SAFE_CHAR_MAP: Record<string, string>;
+/**
+ * 特殊字符映射表（国际化）
+ */
+export declare const SAFE_CHAR_MAP_LOCALE: Record<string, string>;
--- packages/vite-plugin/src/uniapp-x/config.ts
+++ packages/vite-plugin/src/uniapp-x/config.ts
@@ -14,3 +14,21 @@ export const SAFE_CHAR_MAP: Record<string, string> = {
 	"/": "-slash-",
 	":": "-colon-",
 };
+
+/**
+ * 特殊字符映射表（国际化）
+ */
+export const SAFE_CHAR_MAP_LOCALE: Record<string, string> = {
+	"[": "-bracket-start-",
+	"]": "-bracket-end-",
+	"(": "-paren-start-",
+	")": "-paren-end-",
+	"{": "-brace-start-",
+	"}": "-brace-end-",
+	$: "-dollar-",
+	"#": "-hash-",
+	"!": "-important-",
+	"/": "-slash-",
+	":": "-colon-",
+	" ": "-space-",
+};
